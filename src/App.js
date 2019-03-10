7b112d45c21758eaabda267db755e9f6bda6cae9
Gonzalo <arecogonzalo@gmail.com>
2019-03-10 14:34:17 +0800

    finished all the counter and loops

--- src/App.js
+++ src/App.js
@@ -10,19 +10,120 @@ class App extends Component {
     super(props);
 
     this.state = {
-      timer: 1500,
-      counter: "25:00",
-      reset: 1500,
+      timer: 5,
+      counter: "00:05",
+      reset: 5,
       stop: true,
-      pomodoro: true,
-      shortBreak: false,
-      longBreak: false
+      pomodoro: 5,
+      shortBreak: 2,
+      longBreak: 3,
+      loop: 0,
+      isLooping: false
     };
 
     this.countDown = this.countDown.bind(this);
     this.handlePlay = this.handlePlay.bind(this);
     this.handleOptions = this.handleOptions.bind(this);
     this.reset = this.reset.bind(this);
+    this.timeTostring = this.timeTostring.bind(this);
+    this.setOptions = this.setOptions.bind(this);
+    this.looper = this.looper.bind(this);
+  }
+
+  setOptions(current) {
+    const { pomodoro, shortBreak, longBreak, isLooping, loop } = this.state;
+    if (current === "Pomodoro") {
+      this.setState({
+        timer: pomodoro,
+        counter: this.timeTostring(pomodoro),
+        reset: pomodoro,
+        loop: !isLooping ? 0 : loop
+      });
+      clearInterval(this.myTime);
+      this.setState({ stop: true });
+    } else if (current === "Short Break") {
+      this.setState({
+        timer: shortBreak,
+        counter: this.timeTostring(shortBreak),
+        reset: shortBreak,
+        loop: !isLooping ? 0 : loop
+      });
+      clearInterval(this.myTime);
+      this.setState({ stop: true });
+    } else if (current === "Long Break") {
+      this.setState({
+        timer: longBreak,
+        counter: this.timeTostring(longBreak),
+        reset: longBreak,
+        loop: !isLooping ? 0 : loop
+      });
+      clearInterval(this.myTime);
+      this.setState({ stop: true });
+    } else if (current === "Pomodoro Loop") {
+      this.setState({
+        timer: pomodoro,
+        counter: this.timeTostring(pomodoro),
+        reset: pomodoro,
+        loop: 0,
+        isLooping: true
+      });
+      clearInterval(this.myTime);
+      this.setState({ stop: true });
+    }
+  }
+
+  handleOptions(event) {
+    const option = event.target.value;
+    this.setState({ isLooping: false });
+    this.setOptions(option);
+  }
+
+  countDown() {
+    this.myTime = setInterval(() => {
+      this.setState(prevStat => {
+        return { timer: prevStat.timer - 1 };
+      });
+      const { timer } = this.state;
+      this.setState({
+        counter: this.timeTostring(timer),
+        stop: false
+      });
+      /* Stop clock when 0 */
+      const { counter, isLooping } = this.state;
+      if (counter === "00:00" && isLooping) {
+        clearInterval(this.myTime);
+        this.setState(prevStat => {
+          return { loop: prevStat.loop + 1 };
+        });
+        this.looper();
+      } else if (counter === "00:00") {
+        clearInterval(this.myTime);
+      }
+    }, 1000);
+  }
+
+  looper() {
+    const { loop } = this.state;
+    switch (loop) {
+      case 1:
+      case 3:
+      case 5:
+        this.setOptions("Short Break");
+        this.countDown();
+        break;
+      case 2:
+      case 4:
+      case 6:
+        this.setOptions("Pomodoro");
+        this.countDown();
+        break;
+      case 7:
+        this.setOptions("Long Break");
+        this.countDown();
+        break;
+      default:
+        break;
+    }
   }
 
   handlePlay(event) {
@@ -33,56 +134,30 @@ class App extends Component {
     } else if (event.target.value === "stop") {
       clearInterval(this.myTime);
       this.setState({ stop: true });
-    } else {
+    } else if (event.target.value === "reset") {
       clearInterval(this.myTime);
       this.reset();
     }
   }
 
-  countDown() {
-    this.myTime = setInterval(() => {
-      this.setState(prevStat => {
-        return { timer: prevStat.timer - 1 };
-      });
-      const { timer } = this.state;
-      const minutes = parseInt(timer / 60, 10);
-      const seconds = parseInt(timer % 60, 10);
-      this.setState({
-        counter: `${minutes < 10 ? `0${minutes}` : minutes}:${
-          seconds < 10 ? `0${seconds}` : seconds
-        }`,
-        stop: false
-      });
-    }, 1000);
-  }
-
   reset() {
-    const { reset } = this.state;
-    const minutes = parseInt(reset / 60, 10);
-    const seconds = parseInt(reset % 60, 10);
+    const { reset, isLooping } = this.state;
     this.setState({
       timer: reset,
-      counter: `${minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds
-      }`,
-      stop: true
+      counter: this.timeTostring(reset),
+      stop: true,
+      loop: 0,
+      isLooping: !!isLooping
     });
   }
 
-  handleOptions(event) {
-    if (event.target.value === "Pomodoro") {
-      this.setState({ timer: 1500, counter: "25:00", reset: 1500 });
-      clearInterval(this.myTime);
-      this.setState({ stop: true });
-    } else if (event.target.value === "Short Break") {
-      this.setState({ timer: 300, counter: "05:00", reset: 300 });
-      clearInterval(this.myTime);
-      this.setState({ stop: true });
-    } else if (event.target.value === "Long Break") {
-      this.setState({ timer: 600, counter: "10:00", reset: 600 });
-      clearInterval(this.myTime);
-      this.setState({ stop: true });
-    }
+  timeTostring(numbers) {
+    const minutes = parseInt(numbers / 60, 10);
+    const seconds = parseInt(numbers % 60, 10);
+    const total = `${minutes < 10 ? `0${minutes}` : minutes}:${
+      seconds < 10 ? `0${seconds}` : seconds
+    }`;
+    return total;
   }
 
   render() {
--- src/Options.js
+++ src/Options.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Options(props) {
   const { handleOptions } = props;
@@ -7,8 +8,13 @@ function Options(props) {
       <input type="button" value="Pomodoro" onClick={handleOptions} />
       <input type="button" value="Short Break" onClick={handleOptions} />
       <input type="button" value="Long Break" onClick={handleOptions} />
+      <input type="button" value="Pomodoro Loop" onClick={handleOptions} />
     </div>
   );
 }
+
+Options.propTypes = {
+  handleOptions: PropTypes.func.isRequired
+};
 
 export default Options;
